17cd9d576c288976103888ac620b6b6ad7bd7c50
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-06 18:53:08 +0330

    Jwt variables configured

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,6 +3,16 @@ export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     version: '1.0.0',
   },
+  jwt: {
+    access: {
+      secret: process.env.ACCESS_TOKEN_SECRET,
+      exp: process.env.ACCESS_TOKEN_EXPIRATION,
+    },
+    refresh: {
+      secret: process.env.REFRESH_TOKEN_SECRET,
+      exp: process.env.REFRESH_TOKEN_EXPIRATION,
+    },
+  },
   swagger: {
     title: 'Simple Blog API Docs',
     description:
