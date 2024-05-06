8e8e8be1c62b351624fdd89afa0a1c0561e76a45
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-06 19:31:15 +0330

    Public decorator added to auth decorators and bind to signin, signup routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,18 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SignInDto, SignUpDto } from './dtos';
+import { Public } from './decorators';
 
 @ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @ApiOperation({ summary: 'Signs Up a user' })
   @Post('signup')
   signUp(@Body() body: SignUpDto) {
     return this.authService.signUp(body);
   }
 
+  @Public()
   @ApiOperation({ summary: 'Signs in a user' })
   @Post('signin')
   signIn(@Body() body: SignInDto) {
