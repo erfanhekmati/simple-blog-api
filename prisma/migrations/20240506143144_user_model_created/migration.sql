2d2558d6c11b2b6e953f761af24bd10aa6a845c5
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-06 18:03:11 +0330

    User model creation migration added

---
+++ prisma/migrations/20240506143144_user_model_created/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "hashedRt" TEXT,
+    "roles" "Role"[] DEFAULT ARRAY['USER']::"Role"[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
