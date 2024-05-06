0e4dba1be2c5485c66dfb3d4b735e6b9ca32b400
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-06 17:08:20 +0330

    App configured using config file and env vars

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('app.port'));
+
+  Logger.log(
+    `App is listening on port ${configService.get('app.port')} ...`,
+    'Bootstrap',
+  );
 }
 bootstrap();
