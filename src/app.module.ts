77885e7a9ceb3e6084c6289b2ef0cb2647510a9f
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-06 17:57:03 +0330

    Auth module initialized

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -10,6 +11,7 @@ import configuration from './config/configuration';
       load: [configuration],
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
