2eeb7d7e30dc3c59f54f1994fd6f3e5db00bbaff
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-06 18:59:37 +0330

    Sign Up route handler added to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dtos';
 
+@ApiTags('Authentication')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @ApiOperation({ summary: 'Signs Up a user' })
+  @Post('signup')
+  signUp(@Body() body: SignUpDto) {
+    return this.authService.signUp(body);
+  }
+}
