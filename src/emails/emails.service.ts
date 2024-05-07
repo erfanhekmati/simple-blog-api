dfca16fda1c3948c0f82c5d844a9cda3aa0b79c8
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-07 19:51:37 +0330

    Comment added event listener added to emails service

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -2,6 +2,8 @@ import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
 import { Queue } from 'bull';
 import { EmailInterface } from './interfaces/email.interface';
+import { OnEvent } from '@nestjs/event-emitter';
+import { CommentAddedEvent } from 'src/blogs/events';
 
 @Injectable()
 export class EmailsService {
@@ -9,12 +11,13 @@ export class EmailsService {
     @InjectQueue('emailSending') private readonly emailQueue: Queue,
   ) {}
 
-  public sendCommentAddedEmail(email: string, blogId: number) {
+  @OnEvent('comment.added')
+  private sendCommentAddedEmail(payload: CommentAddedEvent) {
     const emailData: EmailInterface = {
       from: 'support@blogapi.com',
-      to: email,
+      to: payload.authorEmail,
       subject: 'Comment on your blog',
-      body: `A comment added to blog with id: ${blogId}.`,
+      body: `A comment added to blog with id: ${payload.blogId}.`,
     };
     this.emailQueue.add('sendTransactionEmail', emailData);
   }
