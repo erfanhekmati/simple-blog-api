ead0233878f17dbfe406b8817620e2460fdc044f
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-07 15:00:02 +0330

    Global validation pipe added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerIntialize } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const configService = app.get(ConfigService);
   SwaggerIntialize(app);
   await app.listen(configService.get('app.port'));
