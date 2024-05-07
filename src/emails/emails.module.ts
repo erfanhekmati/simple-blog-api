06de617fb6a9e70e35f7e5137da614f9f52434e2
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-07 19:11:26 +0330

    Emails module created and emailSending queue registered

---
+++ src/emails/emails.module.ts
@@ -0,0 +1,11 @@
+import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [
+    BullModule.registerQueue({
+      name: 'emailSending',
+    }),
+  ],
+})
+export class EmailsModule {}
