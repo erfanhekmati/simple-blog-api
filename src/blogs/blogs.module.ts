4ee996fc017f4bcadd609a84305659e36a7f8c61
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-07 16:25:53 +0330

    Auth module imported to blogs module

--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { BlogsController } from './blogs.controller';
 import { PrismaService } from 'src/prisma.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [BlogsController],
   providers: [BlogsService, PrismaService],
 })
