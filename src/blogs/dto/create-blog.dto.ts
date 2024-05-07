193cb1e05b7786f7ed3c505b61bc37b640a08c6a
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-07 14:57:31 +0330

    Create blog dto updated

--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -1,1 +1,35 @@
-export class CreateBlogDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  ArrayMaxSize,
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateBlogDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(300)
+  title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(600)
+  description: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(3000)
+  article: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMaxSize(20)
+  tags: string[];
+}
